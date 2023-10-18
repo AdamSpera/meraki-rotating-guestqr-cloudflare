c21c73ec954bebf1a47b1b930d9f56a5da2e77eb
adamspera <adamspera@hotmail.com>
2023-10-18 16:14:51 -0400

    Chnage generate code code

--- functions/getCode.js
+++ functions/getCode.js
@@ -64,7 +64,12 @@ export async function onRequest(context) {
 
 // Helper function to generate a random passphrase
 function generatePassphrase() {
-  const array = new Uint8Array(8);
-  window.crypto.getRandomValues(array);
-  return Array.from(array).map(b => b.toString(16).padStart(2, '0')).join('');
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const minute = String(now.getMinutes()).padStart(2, '0');
+  const second = String(now.getSeconds()).padStart(2, '0');
+  return `${year}${month}${day}${hour}${minute}${second}`;
 }
