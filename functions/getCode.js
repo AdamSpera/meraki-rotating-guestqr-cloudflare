ec0f5e24400b655cf11b33d1ab83fd623b000e69
adamspera <adamspera@hotmail.com>
2023-10-18 13:17:54 -0400

    Add meraki

--- functions/getCode.js
+++ functions/getCode.js
@@ -1,13 +1,37 @@
 export async function onRequest(context) {
   try {
-    // Check if the request method is POST
-    if (context.request.method !== 'GET')
+    // Check if the request method is GET
+    if (context.request.method !== 'GET') {
       throw new Error('Method not allowed');
+    }
 
-    
+    // Define the API credentials and endpoint details
+    const api_key = '9ef04732c76f6e8a53f120aec7442d36398a299d';
+    const network_id = 'N_705376291636925143';
+    const ssid_number = '1';
+    const url = `https://api.meraki.com/api/v1/networks/${network_id}/wireless/ssids/${ssid_number}/identityPsks`;
 
-    // Verify the value of the "verify" field
-    return new Response(JSON.stringify({'ipsk': '123456789'}), { status: 200 });
+    // Make the API request
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-Cisco-Meraki-API-Key': api_key,
+        'Content-Type': 'application/json'
+      },
+    });
+
+    // Check for a successful response
+    if (!response.ok)
+      throw new Error('Error fetching data from Meraki API');
+
+    // Parse the JSON response
+    const jsonResponse = JSON.stringify(await response.json());
+
+    return new Response(jsonResponse, { 
+      status: 200, 
+      headers: { 'Content-Type': 'application/json' } 
+    });
+
   } catch (err) {
     // Return an error response
     const errorResponse = JSON.stringify({ error: err.message });
