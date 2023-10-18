bc1a3e5f7dd3a672a56ab165596ec997f8d16ab1
adamspera <adamspera@hotmail.com>
2023-10-18 16:05:43 -0400

    Add new refactored code

--- functions/getCode.js
+++ functions/getCode.js
@@ -1,3 +1,5 @@
+import crypto from 'crypto';  // Import the crypto module for generating a random passphrase
+
 export async function onRequest(context) {
   try {
     // Check if the request method is GET
@@ -11,37 +13,52 @@ export async function onRequest(context) {
     const ssid_number = '1';
     const url = `https://api.meraki.com/api/v1/networks/${network_id}/wireless/ssids/${ssid_number}/identityPsks`;
 
-    // Make the API request
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'X-Cisco-Meraki-API-Key': api_key,
-        'Content-Type': 'application/json'
-      },
-    });
+    // Helper function to make API requests
+    const makeRequest = async (method, body) => {
+      const options = {
+        method,
+        headers: {
+          'X-Cisco-Meraki-API-Key': api_key,
+          'Content-Type': 'application/json'
+        }
+      };
+      if (body) options.body = JSON.stringify(body);
+      const response = await fetch(url, options);
+      if (!response.ok) throw new Error('Error interacting with Meraki API');
+      return response.json();
+    };
 
-    // Check for a successful response
-    if (!response.ok)
-      throw new Error('Error fetching data from Meraki API');
-
-    // Parse the JSON response
-    const identity_psks = await response.json();
+    // Get the list of IPSKs
+    const identity_psks = await makeRequest('GET');
 
     // Get current date formatted
     const date = new Date().toISOString().slice(0, 10);
-    console.log(`Current date: ${date}`);
+
+    // DEBUG
+    // console.log('date', date);
+    console.log('identity_psks', identity_psks);
 
     // Find the entry with a name matching the current date
     const matchingEntry = identity_psks.find(entry => entry.name === date);
-    console.log(`Matching entry: ${JSON.stringify(matchingEntry)}`);
+
+    let passphrase;
+    if (matchingEntry) {
+      // Entry found, use its passphrase
+      passphrase = matchingEntry.passphrase;
+    } else {
+      // No entry found, generate a new passphrase
+      passphrase = crypto.randomBytes(8).toString('hex');
+      // Create a new IPSK entry
+      const newEntry = { name: date, passphrase };
+      // Post the new entry to the Meraki API
+      await makeRequest('POST', newEntry);
+    }
 
     // Prepare the response
-    const responseBody = matchingEntry ? { ipsk: matchingEntry.passphrase } : false;
-    const jsonResponse = JSON.stringify(responseBody);
-
-    return new Response(jsonResponse, { 
-      status: 200, 
-      headers: { 'Content-Type': 'application/json' } 
+    const jsonResponse = JSON.stringify({ ipsk: passphrase });
+    return new Response(jsonResponse, {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
     });
 
   } catch (err) {
